3e4c61eb159a4ecd0b3e7ea67850157e9007f77f
Pedro Feliu <pedro@medanosol.es>
2020-07-29 09:10:31 +0100

    first commit

---
+++ src/app/shared/breadcrum/breadcrum.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrum',
+  templateUrl: './breadcrum.component.html',
+  styles: [
+  ]
+})
+export class BreadcrumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
