1973c243256a0dc325b67dbbbb7b882a75518e5f
ppfeliuu <ppfeliuu@gmail.com>
2020-08-06 13:51:52 +0100

    06082020

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,14 +8,23 @@ import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  {
+    path: '',
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'grafica1', component: Grafica1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ],
+  },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'grafica1', component: Grafica1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
   { path: '**', component: NopagefoundComponent },
 ];
 
